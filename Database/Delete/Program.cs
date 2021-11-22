dc693aba530f2e8f2927d0d6b1d106ad0ef12968
Joylene98 <58665450+Joylene98@users.noreply.github.com>
2021-11-22 19:00:52 +0000

    Add files via upload

---
+++ Database/Delete/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection cnn;
+            SqlCommand command;
+            int rowsDeleted = 0;
+
+            String connString = "Data Source=.\\sqlexpress; Initial Catalog=Northwind;User ID=Thomas;Password=Thomas";
+            String query = "DELETE FROM Student WHERE StudentID = '1'";
+            cnn = new SqlConnection(connString);
+            cnn.Open();
+            Console.WriteLine("Connection is Open");
+            command = new SqlCommand(query, cnn);
+            
+            if(command.ExecuteNonQuery() == 1) // checks if one record is deleted
+            {
+                rowsDeleted++;
+                Console.WriteLine("Row(s) deleted successfully");
+                Console.WriteLine("Rows Deleted : {0}", rowsDeleted);
+            }
+            Console.ReadLine();
+            command.Dispose();
+            cnn.Close();
+        }
+    }
+}
