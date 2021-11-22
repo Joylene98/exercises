2469d4a28b6ff2af3b79861a43e67616c7f785cf
Joylene98 <58665450+Joylene98@users.noreply.github.com>
2021-11-22 18:59:20 +0000

    Add files via upload

---
+++ Database/Update/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Update
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection cnn;
+            SqlCommand command;
+            int rowsModified = 0;
+            String connString = "Data Source=.\\sqlexpress; Initial Catalog=Northwind; User ID=Thomas;Password=Thomas";
+            String query = "UPDATE Student SET Age = '21' WHERE StudentID = '3'";
+            cnn = new SqlConnection(connString);
+            cnn.Open();
+            Console.WriteLine("Connection is Open");
+            command = new SqlCommand(query, cnn);
+
+            if (command.ExecuteNonQuery() == 1)
+            {
+                rowsModified++;
+                Console.WriteLine("Row(s) Updated Sucessfully!");
+                Console.WriteLine("Row(s) Modified : {0}", rowsModified);
+            }
+            Console.ReadLine();
+            command.Dispose();
+            cnn.Close();
+            
+
+        }
+    }
+}
